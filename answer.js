edc56a5f0220d6520f41d65cf990dab6b06b6ded
Judy Gonzalez <judy@thearrayschool.com>
2018-12-13 14:44:59 -0700

    Here is my final answer

--- answer.js
+++ answer.js
@@ -1,1 +1,21 @@
 // *** YOUR ANSWER BELOW ***
+
+const beerEnd = " bottles of beer."
+const beer = " bottles of beer"
+const wall = " on the wall."
+const takeOne = " You take one down, pass it around. "
+
+function beerSong(n){
+  let line = "";
+
+    for(let i = n; i >= 1; i--){
+
+        line += i + beer + wall + takeOne;
+
+         let oneDown = i - 1;
+
+        line += oneDown + beerEnd + "\n";
+    }  return console.log(line);
+}
+
+beerSong(99)
